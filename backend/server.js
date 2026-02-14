969caa1e6cc29dfe6fa1a60454413a2d26091696
Ismail Abed <abed200328.z@gmail.com>
2026-02-14 17:21:24 +0200

    Backend setup with Express and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+console.log("ENV URI:", process.env.MONGO_URI);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });

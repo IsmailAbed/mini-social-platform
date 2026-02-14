26498cd6dae832cc87bfb4db28286cebeb13f132
Ismail Abed <abed200328.z@gmail.com>
2026-02-14 17:38:37 +0200

    Frontend setup with authentication and feed integration

---
+++ frontend/js/feed.js
@@ -0,0 +1,35 @@
+async function loadPosts() {
+  const posts = await apiRequest("/posts");
+
+  const postsContainer = document.getElementById("posts");
+  postsContainer.innerHTML = "";
+
+  posts.forEach(post => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+      <strong>${post.user.username}</strong>
+      <p>${post.content}</p>
+      <small>${post.likes.length} likes</small>
+      <br>
+      <button onclick="likePost('${post._id}')">Like</button>
+    `;
+    postsContainer.appendChild(div);
+  });
+}
+
+async function createPost() {
+  const content = document.getElementById("postContent").value;
+
+  await apiRequest("/posts", "POST", { content });
+
+  document.getElementById("postContent").value = "";
+  loadPosts();
+}
+
+async function likePost(id) {
+  await apiRequest(`/posts/${id}/like`, "PUT");
+  loadPosts();
+}
+
+loadPosts();
